9af50d1309e2c437b5d2c906cc04f6d25575af63
Jasmeet Brar <jasmeet.brar@hotmail.com>
2021-03-23 17:02:51 -0400

    Fixed the second query by correctly converting the units used in the get_used_memory_percentage function"

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -9,14 +9,14 @@ END;
 $$
     LANGUAGE PLPGSQL;
 
--- This function returns the free memory percentage for the given memory amount and host id.
-CREATE OR REPLACE FUNCTION get_free_memory_percentage(memory INTEGER, host_id INTEGER) RETURNS NUMERIC AS
+-- This function returns the free memory percentage for the given used memory amount and host id.
+CREATE OR REPLACE FUNCTION get_used_memory_percentage(free_memory INTEGER, host_id INTEGER) RETURNS NUMERIC AS
 $$
 DECLARE
     total_memory INTEGER;
 BEGIN
-    SELECT total_mem INTO total_memory FROM host_info WHERE id = host_id;
-    RETURN (total_memory - memory) * 100 / total_memory;
+    SELECT (total_mem / 1000) INTO total_memory FROM host_info WHERE id = host_id;
+    RETURN (total_memory - free_memory) * 100 / total_memory;
 END;
 $$
     LANGUAGE PLPGSQL;
@@ -41,7 +41,7 @@ SELECT
     CAST(timestamp AS timestamp)
   ) AS timestamp,
   AVG(
-    get_free_memory_percentage(memory_free, host_id)
+    get_used_memory_percentage(memory_free, host_id)
   ) AS avg_used_mem_percentage
 FROM
   host_usage
