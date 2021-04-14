308d60e88a8e59bfde34696d254c35680172f1f6
Jasmeet Brar <jasmeet.brar@hotmail.com>
2021-04-14 16:54:50 -0400

    Initialized unit test code for TwitterService

---
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/service/TwitterServiceUnitTest.java
@@ -0,0 +1,30 @@
+package ca.jrvs.apps.twitter.service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TwitterServiceUnitTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void postTweet() {
+    }
+
+    @Test
+    public void showTweet() {
+    }
+
+    @Test
+    public void deleteTweets() {
+    }
+}
