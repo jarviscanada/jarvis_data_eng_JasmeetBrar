b83aa1fa5697bb0877c8b3174e2cb484180cc567
Jasmeet Brar <jasmeet.brar@hotmail.com>
2021-04-14 11:53:26 -0400

    Fixed the DAO integration tests

--- core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/TwitterDaoIntTest.java
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/TwitterDaoIntTest.java
@@ -4,9 +4,7 @@ import ca.jrvs.apps.twitter.dao.helper.HttpHelper;
 import ca.jrvs.apps.twitter.dao.helper.TwitterHttpHelper;
 import ca.jrvs.apps.twitter.model.Coordinates;
 import ca.jrvs.apps.twitter.model.Tweet;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 
 import static org.junit.Assert.*;
 
@@ -14,17 +12,12 @@ import java.util.ArrayList;
 
 public class TwitterDaoIntTest {
 
-    private static TwitterDao twitterDao;
+    private TwitterDao twitterDao;
+    private Tweet tweet;
+    private Long id;
 
-    private static Tweet tweet1;
-    private static Tweet tweet2;
-    private static Tweet tweet3;
-
-    private static Long createTweetId;
-    private static Long showTweetId;
-
-    @BeforeClass
-    public static void setUp() {
+    @Before
+    public void setUp() throws Exception {
         String consumerKey = System.getenv("consumerKey");
         String consumerSecret = System.getenv("consumerSecret");
         String accessToken = System.getenv("accessToken");
@@ -33,101 +26,49 @@ public class TwitterDaoIntTest {
         HttpHelper httpHelper = new TwitterHttpHelper(consumerKey, consumerSecret, accessToken, tokenSecret);
         twitterDao = new TwitterDao(httpHelper);
 
-        tweet1 = new Tweet();
-        tweet1.setText("Test 1");
+        tweet = new Tweet();
+        tweet.setText("Test");
 
-        Coordinates coordinates1 = new Coordinates();
-        coordinates1.setCoordinates(new ArrayList<Double>(){{
+        Coordinates coordinates = new Coordinates();
+        coordinates.setCoordinates(new ArrayList<Double>(){{
             add(48.7);
             add(2.0);
         }});
-        tweet1.setCoordinates(coordinates1);
 
-        tweet2 = new Tweet();
-        tweet2.setText("Test 2");
-
-        Coordinates coordinates2 = new Coordinates();
-        coordinates2.setCoordinates(new ArrayList<Double>(){{
-            add(50.0);
-            add(10.0);
-        }});
-        tweet2.setCoordinates(coordinates2);
-
-        tweet3 = new Tweet();
-        tweet3.setText("Test 3");
-
-        Coordinates coordinates3 = new Coordinates();
-        coordinates3.setCoordinates(new ArrayList<Double>(){{
-            add(55.0);
-            add(5.0);
-        }});
-        tweet3.setCoordinates(coordinates3);
+        tweet.setCoordinates(coordinates);
     }
 
-    @AfterClass
-    public static void tearDown() {
-        if(createTweetId != null) {
-            twitterDao.deleteById(createTweetId.toString());
-        }
-        if(showTweetId != null) {
-            twitterDao.deleteById(showTweetId.toString());
-        }
-    }
-
-    @Test
-    public void createTweet() throws Exception {
-        Tweet response = twitterDao.create(tweet1);
-        Double longitude = response.getCoordinates().getCoordinates().get(0);
-        Double latitude = response.getCoordinates().getCoordinates().get(1);
-
-
-        assertNotNull(response.getId());
-
-        createTweetId = response.getId();
-
-        assertEquals(response.getText(), "Test 1");
-        assertEquals(latitude, 48.7, 0.1);
-        assertEquals(longitude, 2.0, 0.1);
-    }
-
-    @Test
-    public void showTweet() throws Exception {
-        Tweet createResponse = twitterDao.create(tweet2);
-        Long id = createResponse.getId();
-
-        showTweetId = id;
-
+    @After
+    public void tearDown() throws Exception {
         assertNotNull(id);
-
-        Tweet response = twitterDao.findById(id.toString());
-
-        Double longitude = response.getCoordinates().getCoordinates().get(0);
-        Double latitude = response.getCoordinates().getCoordinates().get(1);
-
-        assertEquals(response.getText(), "Test 2");
-        assertEquals(latitude, 50.0, 0.1);
-        assertEquals(longitude, 10.0, 0.1);
-    }
-
-    @Test
-    public void deleteTweet() throws Exception {
-        Tweet createResponse = twitterDao.create(tweet3);
-        Long id = createResponse.getId();
-
-        assertNotNull(id);
-
         Tweet response = twitterDao.deleteById(id.toString());
 
+        // Test that deleting a tweet worked
+        testResponse(response);
+    }
+
+    @Test
+    public void createAndFindTweet() {
+        Tweet response = twitterDao.create(tweet);
+
+        // Test that creating a tweet worked
+        testResponse(response);
+
+        id = response.getId();
+        response = twitterDao.findById(id.toString());
+
+        // Test that finding a tweet worked
+        testResponse(response);
+    }
+
+    private void testResponse(Tweet response) {
         Double longitude = response.getCoordinates().getCoordinates().get(0);
         Double latitude = response.getCoordinates().getCoordinates().get(1);
 
-        assertEquals(response.getText(), "Test 3");
-        assertEquals(latitude, 55.0, 0.1);
-        assertEquals(longitude, 5.0, 0.1);
-
-        Tweet showResponse = twitterDao.findById(id.toString());
-
-        assertNull(showResponse.getId());
+        assertNotNull(response.getId());
+        assertEquals(response.getText(), "Test");
+        assertEquals(latitude, 48.7, 0.1);
+        assertEquals(longitude, 2.0, 0.1);
     }
 
 }
